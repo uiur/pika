335c1735b1bc62222d12964cc7f8cbe954323e12
Kazato Sugimoto <uiureo@gmail.com>
2018-05-25 18:08:10 +0900

    Add api call sample

--- client/src/index.js
+++ client/src/index.js
@@ -1,1 +1,14 @@
-console.log('foobar')
+'use strict'
+
+import Frisbee from 'frisbee'
+
+const api = new Frisbee({
+  baseURI: 'http://localhost:3000'
+})
+
+async function main () {
+  const res = await api.get('/wallet')
+  console.log(JSON.stringify(res.body))
+}
+
+main()
